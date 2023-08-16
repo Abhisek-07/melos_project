dbc40be6d6feb3fea38ff738b3107c6952ef2a4b
Abhisek-Dash-open <abhisek.dash@bankopen.co>
2023-08-16 17:49:10 +0530

    changed routes_list.dart to hookconsumerwidget from statefulHookConsumerWidget

--- apps/bus_routes_app/Bus_Routes_Modified/lib/widgets/routes_list.dart
+++ apps/bus_routes_app/Bus_Routes_Modified/lib/widgets/routes_list.dart
@@ -1,153 +1,22 @@
-// import 'dart:async';
-
-// import 'package:bus_routes_app/models/bus_routes.dart';
 import 'package:bus_routes_app/providers/routes_provider.dart';
 import 'package:core/core.dart';
-// import 'package:bus_routes_app/utils/notification_service.dart';
 import 'package:bus_routes_app/widgets/routes_card.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-// import 'package:workmanager/workmanager.dart';
-
-// import 'package:bus_routes_app/utils/utils.dart';
-// import 'package:bus_routes_app/utils/shared_preferences_helper.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// made global variable to use in workmanager and by this screen, the busRoutes list is available
-// List<BusRoute> sortedRoutes = [];
-// Workmanager workmanager = Workmanager();
 final timeFormat = DateFormat('HH:mm');
 
-// // method executed by workmanager
-// void callbackDispatcher() {
-//   workmanager.executeTask((task, inputData) async {
-//     if (task == "sortRoutesTask") {
-//       sortedRoutes =
-//           await SharedPreferencesHelper.getSortedRoutesFromSharedPreferences();
-//       sortedRoutes = sortRoutesByTime(sortedRoutes);
-
-//       if (sortedRoutes.isNotEmpty &&
-//           sortedRoutes[0].shortestTripStartTime != null) {
-//         final remainingTime =
-//             getRemainingTimeInMinutes(sortedRoutes[0].shortestTripStartTime!);
-//         // NotificationService notificationService = NotificationService();
-//         // await notificationService.init();
-//         await NotificationService.init();
-//         NotificationService.showNotification(
-//             sortedRoutes[0].name, remainingTime);
-//       }
-
-//       await SharedPreferencesHelper.saveSortedRoutesToSharedPreferences(
-//           sortedRoutes);
-//     }
-
-//     return Future.value(true);
-//   });
-// }
-
-class RoutesList extends StatefulHookConsumerWidget {
+class RoutesList extends HookConsumerWidget {
   const RoutesList({
     super.key,
-    // required this.busRoutes,
     required this.scrollerController,
   });
 
   final ScrollController scrollerController;
-  // final List<BusRoute> busRoutes;
 
   @override
-  ConsumerState<RoutesList> createState() => _RoutesListState();
-}
-
-class _RoutesListState extends ConsumerState<RoutesList> {
-  // Timer? timer;
-
-  // NotificationService notificationService = NotificationService();
-
-  // bool isLoading = false;
-
-  // final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
-  //     GlobalKey<RefreshIndicatorState>();
-
-  // timer is initialised inside initstate and first call to updateData to sort the routes, notifications and work manager are also initialized
-  @override
-  void initState() {
-    super.initState();
-    // updateData();
-    // startTimer();
-    // initializeNotifications();
-    // configureWorkManager();
-  }
-
-  // void initializeNotifications() async {
-  //   await notificationService.init();
-  // }
-
-  // // for configuring the work manager
-  // void configureWorkManager() async {
-  //   await workmanager.initialize(
-  //     callbackDispatcher,
-  //     isInDebugMode: true,
-  //   );
-  //   workmanager.registerPeriodicTask(
-  //     "sortRoutesTask",
-  //     "sortRoutesTask",
-  //     frequency: const Duration(minutes: 1),
-  //     initialDelay: const Duration(seconds: 2),
-  //   );
-  // }
-
-  // dispose method
-  @override
-  void dispose() {
-    // timer?.cancel();
-    super.dispose();
-  }
-
-  // // updates sorted list of bus routes
-  // void updateData() async {
-  //   setState(() {
-  //     sortedRoutes = sortRoutesByTime(widget.busRoutes);
-  //   });
-
-  //   // saving sorted routes to shared preferences
-  //   await SharedPreferencesHelper.saveSortedRoutesToSharedPreferences(
-  //       sortedRoutes);
-
-  //   // logic for showing notifications when 5 minutes till next bus
-  //   if (sortedRoutes.isNotEmpty &&
-  //       sortedRoutes[0].shortestTripStartTime != null) {
-  //     final remainingTime =
-  //         getRemainingTimeInMinutes(sortedRoutes[0].shortestTripStartTime!);
-
-  //     if (remainingTime == 5) {
-  //       notificationService.showNotification(
-  //           sortedRoutes[0].name, remainingTime);
-  //     }
-  //   }
-  // }
-
-  // // starts timer for periodic update of bus routes every minute
-  // void startTimer() {
-  //   timer = Timer.periodic(const Duration(minutes: 1), (Timer timer) {
-  //     updateData();
-  //   });
-  // }
-
-  // // function to be executed on pull to refresh
-  // Future<void> _refreshList() async {
-  //   setState(() {
-  //     isLoading = true;
-  //   });
-
-  //   await Future.delayed(const Duration(milliseconds: 1500));
-
-  //   isLoading = false;
-  //   updateData();
-  // }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     RoutesNotifier routesNotifier = ref.watch(routesProvider);
 
     if (routesNotifier.isRefreshing) {
@@ -164,11 +33,9 @@ class _RoutesListState extends ConsumerState<RoutesList> {
 
     return RefreshIndicator(
       key: routesNotifier.refreshIndicatorKey,
-      // _refreshIndicatorKey,
       onRefresh: routesNotifier.refreshList,
-      // _refreshList,
       child: ListView.builder(
-          controller: widget.scrollerController,
+          controller: scrollerController,
           itemCount: routesNotifier.sortedBusRoutes.length,
           itemBuilder: (context, index) {
             final BusRoute route = routesNotifier.sortedBusRoutes[index];
